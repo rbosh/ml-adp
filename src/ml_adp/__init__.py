964514cea76f7a0ddf315dbd0dfd0cfa65168f26
Ruben Wiedemann <r.wiedemann@campus.lmu.de>
2022-01-11 22:23:43 +0100

    Update Pytorch

--- src/ml_adp/__init__.py
+++ src/ml_adp/__init__.py
@@ -23,4 +23,4 @@ r"""Machine learning based ADP for finite-horizon discrete-time stochastic optim
     the data $k=(k_0,\dots, k_T)$ in :class:`ml_adp.cost.CostToGo` to provide the numerical simulation of the total cost.
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
