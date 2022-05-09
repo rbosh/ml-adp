eaa732914613f7afbad616f8040e6b8bc39fa7fc
Ruben Wiedemann <r.wiedemann@campus.lmu.de>
2022-05-09 05:33:15 +0200

    Documentation Improvements

--- src/ml_adp/mapping/convex.py
+++ src/ml_adp/mapping/convex.py
@@ -11,39 +11,35 @@ FFNSize = Sequence[SpaceSize]
 class PICNN(torch.nn.Module):
     r"""Partially Input-Convex Neural Network Architecture (PICNN)
     
-    Structurally, a composite of
+    Structurally, the composite of
     
-    * an *output net* of size $(k_0,\dots, k_{J+1})$        
-        * activation functions $g_0,\dots, g_J$, 
-        * propagation :class:`Layer`s $A_0,\dots, A_J$ of sizes $A_j\colon\mathbb{R}^{k_j}\to\mathbb{R}^{k_{j+1}}$ with a common constraint function $\phi\mathbb{R}\to M$ with non-negative range $M$
-        * residual connection :class:`Layer`'s $B_0,\dots, B_J$ with sizes $B_j\mathbb{R}^{k_0}\to\mathbb{R}^{k_{j+1}}$
+    * an *output net* of size $(k_0,\dots, k_{J+1})$ ultimately producing the output of the architecture and consisting of
+            
+        * propagation :class:`Layer`'s $A_0,\dots, A_J$ of sizes $A_j\colon\mathbb{R}^{k_j}\to\mathbb{R}^{k_{j+1}}$ with a common constraint function $\phi\colon\mathbb{R}\to \mathbb{R}_{\geq 0}$ with non-negative range
+        * residual connection :class:`Layer`'s $B_0,\dots, B_J$ of sizes $B_j\colon\mathbb{R}^{k_0}\to\mathbb{R}^{k_{j+1}}$
+        * an activation function $\sigma$ to use for the hidden layers as well as an activation function $\rho$ to use for the output layer, both convex and increasing
         
-    and ultimately producing the output of the architecture
-    * a *parameter net*, an :class:`FFN` $L=(L_0,\dots, L_J)$ of size $(p_0,\dots,p_{J+1})$, processing the parameter value
-    * *parameter heads*, $\ReLU$-:class:`Layer`'s $U_0,\dots, U_J$ and $\id_\R$-layers $V_0,\dots, V_J$, $W_0,\dots, W_J$, with sizes
-    $$U_j\colon\reals{p_j}\to\reals{k_j},\quad V_j\colon\reals{p_j}\to \reals{k_0},\quad W_j\colon\reals{p_j}\to \reals{k_{j+1}}$$ 
-    and no biases, integrating the parameter net forward propagation results into the output net propagation
+    * a *parameter net*, an :class:`FFN` $L=(L_0,\dots, L_J)$ of size $(p_0,\dots,p_{J+1})$, processing the given parameter
+    * *parameter heads*, that is `Layer`'s $U_0,\dots, U_J$ with a common activation function $\psi\colon\mathbb{R}\to\mathbb{R}_{\geq 0}$ with non-negative range (called *floor function*) and layers $V_0,\dots, V_J$, $W_0,\dots, W_J$, with sizes $$U_j\colon\mathbb{R}^{p_j}\to\mathbb{R}^{k_j},\quad V_j\colon\mathbb{R}^{p_j}\to \mathbb{R}^{k_0},\quad W_j\colon\mathbb{R}^{p_j}\to \mathbb{R}^{k_{j+1}}$$ and no biases, integrating the parameter net forward propagation results into the output net propagation.
     
-    As a callable, implements
+    As a callable, implements the function
     $$f\colon (u,\eta)\mapsto v$$
-    where $v = u_{J+1}$ is the final result produced by the forward propagation $u_0 = u$
+    of input $u$, parameter $\eta$ and of output $v = u_{J+1}$ the final result of the forward propagation of $u_0 = u$ in
     $$u_{j+1} = g_j(A_j(u_j\odot \eta^{(U)}_j) + B_j(u_0\odot \eta^{(V)}_j) + \eta^{(W)}_j),\quad j=0,\dots, J$$
-    in which
+    where
     $$\eta^{(U)}_j = U_j(\eta_j),\quad \eta^{(V)}_j = V_j(\eta_j),\quad \eta^{(W)}_j = W_j(\eta_j)$$
-    and $(\eta_j)_j$ is itself the forward propagation of $\eta$ in $L$, meaning $\eta_0 = \eta$ and $\eta_{j+1} = L_j(\eta_j)$ for $j=0,\dots, J$. 
+    and $(\eta_j)_j$ is itself the forward propagation of $\eta_0 = \eta$ in $L$ (meaning $\eta_{j+1} = L_j(\eta_j),\quad j=0,\dots, J$). 
     
-    It can be shown that for all parameters $\eta$ the function
-    $$f_\eta = f(\var, \eta)\colon \mathbb{R}^{k_0}\to\mathbb{R}^{k_{J+1}}, u\mapsto f(u,\eta)$$
-    is convex, provided that
+    It can be shown that for all parameters $\eta$ the partial function
+    $$f_\eta = f(\cdot, \eta)\colon \mathbb{R}^{k_0}\to\mathbb{R}^{k_{J+1}},\quad u\mapsto f(u,\eta)$$
+    is convex.
+    For this to be the case it is indeed crucial that
     
-    * the activation functions of the output net are convex and increasing (this is the case for most common activation functions including $\mathrm{ReLU}$ and $\mathrm{ELU}$)
-    * the constraint functions of linearities of the layers $A_0,\dots, A_J$ have non-negative range (meaning their transformation weights are constrained to have non-negative entries only, see the documentation of :class:`Linear`)
-    * the activation functions of the parameter head layers $U_0,\dots, U_J$ have non-negative range
-    * no affine batch norms
-    
-    which as conditions might as well be included into the definition of PICNN's.
-    
-    Introduced in `Amos et al.`_, the architecture can be expected to have satisfying approximation capabilties.
+    * the activation functions of the output net all are convex and increasing (this is the case for most common activation functions including $\mathrm{ReLU}$ and $\mathrm{ELU}$)
+    * the constraint functions of the linearities of the layers $A_0,\dots, A_J$ have non-negative range (meaning their transformation weights are constrained to have non-negative entries only, see the documentation of :class:`Linear`) and these layer's batch norms do not have affine parameters
+    * the floor function, i.e. the activation function of the parameter head layers $U_0,\dots, U_J$, has non-negative range
+        
+    Introduced in `Amos et al.`_, the architecture has universal approximation capabilities in the class of families of convex functions.
     
     .. _Amos et al.: https://arxiv.org/abs/1609.07152
     """
@@ -56,13 +52,13 @@ class PICNN(torch.nn.Module):
                  output_net_output_activation: Callable = None,
                  floor_func: Optional[Callable] = None,
                  constraint_func: Optional[Callable] = None,
-                 param_net_config: Optional[Dict] = None,
-                 residual_layers_config: Optional[Dict] = None,
                  propagation_layers_config: Optional[Dict] = None,
+                 residual_layers_config: Optional[Dict] = None,
+                 param_net_config: Optional[Dict] = None,
                  parameter_heads_config: Optional[Dict] = None) -> None:
         r"""Construct a PICNN Instance
         
-        To construct an instance specify the size $(k_0,\dots, k_{J+1})$ of the output net and the size $(p_0,\dots, p_{J+1})$ of the parameter net using `output_net_size` and `param_net_size`, respectively.
+        To construct an instance necessarily specify the size $(k_0,\dots, k_{J+1})$ of the output net and the size $(p_0,\dots, p_{J+1})$ of the parameter net and optionally configure the additional parameters.
         
         
         Parameters
@@ -72,21 +68,21 @@ class PICNN(torch.nn.Module):
         param_net_size : FFNSize
             The size $(p_0,\dots, p_{J+1})$ of the parameter net
         output_net_hidden_activation : Optional[Callable]
-            The increasing convex activation function to be used for the hidden layers of the output net; optional, default None (indicates usage of $\mathrm{ReLU}$)
+            The increasing convex activation function $\sigma$ to be used for the hidden layers of the output net; optional, default ``None`` (indicates usage of $\mathrm{ReLU}$-activation function)
         output_net_output_activation : Optional[Callable]
-            The increasing convex activation function to be used for the output layer of the output net; optional, default None (indicates no activation function)
+            The increasing convex activation function $\rho$ to be used for the output layer of the output net; optional, default ``None`` (indicates no activation function)
         floor_func : Optional[Callable]
-            The activation function with non-negative range to be used for the parameter head layers $U_0,\dots, U_J$; optional, default None (indicates $\mathrm{ReLU}$)
+            The floor function, i.e. the activation function $\psi$ with non-negative range to be used for the parameter head layers $U_0,\dots, U_J$; optional, default ``None`` (indicates $\mathrm{ReLU}$)
         constraint_func : Optional[Callable]
-            The constraint function with non-negative range to be used for the output nets propagation layers's linearities; optional, default None (indicates $\exp$)
-        param_net_config : Optional[Dict]
-            FFN-configuration dict of the parameter net, passed to the :class:`FFN`-constructor of the parameter net; optional, default None (indicating not to pass any configuration leading to the default parameters)
-        residual_layers_config : Optional[Dict]
-            Layer-configuarion dict to use to construct the residual layers $B_0,\dots, B_J$, `activation`-key's value will be updated with `floor_func`, `bias`-key's value will be updated with `False`; optional, default None
+            The constraint function $\phi$ with non-negative range to be used for the output nets propagation layers' linearities; optional, default None (indicates $\exp$)
         propagation_layers_config : Optional[Dict]
-            Layer-configuarion dict to use to construct the propagation layers $A_0,\dots, A_J$, `constraint_func`-key's value will be updated with `constraint_func` and `batch_norm_affine` will be updated with `False`; optional, default None
+            Layer-configuarion to use to construct the propagation layers $A_0,\dots, A_J$; the ``constraint_func``-value will be updated with the given ``constraint_func`` and the ``batch_norm_affine``-value will be updated with ``False``; optional, default ``None``
+        residual_layers_config : Optional[Dict]
+            Layer-configuarion to use to construct the residual layers $B_0,\dots, B_J$; the ``activation``-value will be updated with the given ``floor_func``, the ``bias``-value will be updated with `False`; optional, default ``None``
         parameter_heads_config : Optional[Dict]
-            Layer-configuration dict to be used to construct the parameter heads $U_0,\dots, U_J$, $V_0,\dots, V_J$, $W_0,\dots, W_J$; for the construction of $U_0,\dots, U_J$, the `activation`-key's value will be updated with `floor_func`, `bias`-key's value will be updated with `False`; optional, default None
+            Layer-configuration to use  to construct the parameter heads $U_0,\dots, U_J$, $V_0,\dots, V_J$, $W_0,\dots, W_J$; for the construction of $U_0,\dots, U_J$, the ``activation``-value will be updated with ``floor_func``, ``bias``-key's value will be updated with ``False``; optional, default ``None``
+        param_net_config : Optional[Dict]
+            FFN-configuration to use to construct the parameter net, passed directly to the :class:`FFN`-constructor; optional, default ``None`` (indicating not to pass any configuration, leading to the default FFN parameters)
         
         Raises
         ------
