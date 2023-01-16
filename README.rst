7c640e90dda0b2251a2f846d6329c57d2e18350c
Ruben <rw5739@gmail.com>
2023-01-16 22:30:06 +0100

    Simplify Readme, Improve Documentation

--- README.rst
+++ README.rst
@@ -2,8 +2,8 @@
 ml-adp
 ******
 
-``ml_adp`` is a Python package embedding into the Pytorch neural network and optimization framework and serves the numerical solution of Markovian discrete-time finite-horizon stochastic optimal control problems.
-It exports a list-like interface to the central functional components of such optimal control problems, allowing for concise implementations of numerical methods that rely on the approximate satisfaction of the discrete-time Bellman equations (Approximate Dynamic Programming; ADP).
+``ml_adp`` embeds into the Pytorch neural network and optimization framework and serves the numerical solution of Markovian finite-horizon discrete-time stochastic optimal control problems.
+It interfaces such optimal control problems in a list-like manner, allowing for concise implementations of numerical methods that rely on the approximate satisfaction of the discrete-time Bellman equations (Approximate Dynamic Programming; ADP).
 
 Documentation is available `here`__.
 
--- src/ml_adp/__init__.py
+++ src/ml_adp/__init__.py
@@ -3,9 +3,9 @@ r"""Machine learning based ADP for finite-horizon discrete-time stochastic optim
     An optimal control problem within the scope of :mod:`ml_adp` of $T$ *steps* consists of $T+1$ *state functions*
     $$F_0(s_0,a_0,\xi_1),\dots ,F_T(s_T, a_T, \xi_{T+1}),$$
     together with $T+1$ *cost functions*
-    $$K_0(s_0, a_0, \xi_0),\dots, K_T(s_T, a_T).$$
+    $$K_0(s_0, a_0),\dots, K_T(s_T, a_T).$$
 
-    Given an initial value $S_0$ and independent "random effects" $\Xi_1,\dots, \Xi_{T+1}$, a "dynamic" choice of controls $A_0,\dots, A_T$ defines a controlled random *state evolution*
+    Given an initial value $S_0$ and independent "random effects" $\Xi_1,\dots, \Xi_{T+1}$, a "dynamic" choice of controls $A_0,\dots, A_T$ defines a random *controlled state evolution*
     $$S_{t+1} = F_{t+1}(S_t, A_t, \Xi_{t+1}), \quad t=0,\dots, T$$
     with which the random *total cost*
     $$K^{F, A}(S_0, \Xi_1, \dots, \Xi_{T+1}) = \sum_{t=0}^T K_t(S_t, A_t)$$
@@ -14,7 +14,7 @@ r"""Machine learning based ADP for finite-horizon discrete-time stochastic optim
 
     The theory establishes in sufficiently well-behaved situations that choices of controls
     in the form of *control functions* $A_0(s_0), \dots, A_T(s_T)$ (via $A_t = A_t(S_t)$)
-    from function classes within which common neural network architectures have universal approximation capabilities exhibit the required dynamicity and are not restricted in regard to their potential optimality.
+    from function classes within which common neural network architectures have universal approximation capabilities exhibit the required dynamicity and are potentially optimal.
 
     :mod:`ml_adp` wraps the state functions $F=(F_0,\dots, F_T)$ and a choice of controls $A=(A_0,\dots, A_T)$ into :class:`ml_adp.cost.Propagator`'s to provide the numerical simulation of the state evolution and bundles a such :class:`ml_adp.cost.Propagator`-instance with the data $K=(K_0,\dots, K_T)$ into :class:`ml_adp.cost.CostToGo`'s to provide the numerical simulation of the total cost.
 """
